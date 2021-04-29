8c7d42a800b83a34de54a76b4a80ff6bcd203a35
Cem Karakurt <cem@11sight.com>
2021-04-29 14:00:16 +0300

    added new configs

--- custom.js
+++ custom.js
@@ -2,11 +2,12 @@ $( document ).ready(function() {
     $('#create-url').on('click',function(){
         let urlParams = "#"
         const inputCount = $('input[type=text]').length
-        $('input[type=text]').each(function(){
+        $('input, select').each(function(){
             const field = $(this)
             const val = field.val();
             const confName = $(this).attr('id')
-
+            console.log(confName)
+            console.log(val)
             if(val){
                 if(urlParams != "#")
                     urlParams+= '&'
